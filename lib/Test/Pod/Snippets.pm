11884fa00b70f36bf57a6f1efb1845098ee3ff7c
Yanick Champoux <yanick+cpan@babyl.dyndns.org>
2008-08-05 22:40:54 +0000

    Import of YANICK/Test-Pod-Snippets-0.06 from CPAN.
    gitpan-cpan-distribution: Test-Pod-Snippets
    gitpan-cpan-version:      0.06
    gitpan-cpan-path:         YANICK/Test-Pod-Snippets-0.06.tar.gz
    gitpan-cpan-author:       YANICK
    gitpan-cpan-maturity:     released

--- lib/Test/Pod/Snippets.pm
+++ lib/Test/Pod/Snippets.pm
@@ -9,7 +9,7 @@ use Test::Pod::Snippets::Parser;
 use Module::Locate qw/ locate /;
 use Params::Validate qw/ validate_with validate /;
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 #<<<
 my @parser_of   :Field :Get(parser);
@@ -34,6 +34,12 @@ my @do_functions :Field
                  :Get(is_extracting_functions)
                  :Set(extracts_functions)
                  ;
+
+my @preserve_lines :Field
+                   :Default(1)
+                   :Arg(preserve_lines)
+                   :Std(preserve_lines)
+                   ;
 #>>>
                  
 my @object_name  :Field :Default('$thingy') :Arg(object_name);
@@ -500,6 +506,12 @@ will produces
 The name of the object (with the leading '$') to be
 used for the methods if the T:P:S object is set to 
 extract methods.
+
+=item preserve_lines => I<$boolean>
+
+If sets to true (which is the default), the generated code
+will be peppered with '#line' pre-compiler lines that will
+have any failing test point to the test's original file.
 
 =back
 
